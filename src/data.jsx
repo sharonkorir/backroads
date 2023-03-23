a4ad1a916f15b12a51c7d60867b2856f830ae7ff
sharonkorir <sharon.korir@student.moringaschool.com>
2023-03-23 16:03:25 +0300

    Fix: make tours and services dynamic data

--- src/data.jsx
+++ src/data.jsx
@@ -1,3 +1,10 @@
+import tour1 from "./assets/images/tour-1.jpeg";
+import tour2 from "./assets/images/tour-2.jpeg";
+import tour3 from "./assets/images/tour-3.jpeg";
+import tour4 from "./assets/images/tour-4.jpeg";
+import tour5 from "./assets/images/tour-5.jpeg";
+import tour6 from "./assets/images/tour-6.jpeg";
+
 export const pageLinks = [
   { id: 1, href: "#home", text: "home" },
   { id: 2, href: "#services", text: "services" },
@@ -9,4 +16,88 @@ export const socialLinks = [
   { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
   { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
   { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+];
+
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "as fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    image: tour1,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    duration: 6,
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quo exercitationem fugit, qui corporis.",
+    cost: 2100,
+    location: "china",
+  },
+  {
+    id: 2,
+    image: tour2,
+    date: "october 1th, 2020",
+    title: "best of java",
+    duration: 11,
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quo exercitationem fugit, qui corporis.",
+    cost: 1400,
+    location: "indonesia",
+  },
+  {
+    id: 3,
+    image: tour3,
+    date: "september 15th, 2020",
+    title: "explore hong kong",
+    duration: 8,
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quo exercitationem fugit, qui corporis.",
+    cost: 5000,
+    location: "hong kong",
+  },
+  {
+    id: 4,
+    image: tour4,
+    date: "december 5th, 2019",
+    title: "kenya highlights",
+    duration: 20,
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quo exercitationem fugit, qui corporis.",
+    cost: 3300,
+    location: "kenya",
+  },
+  {
+    id: 5,
+    image: tour5,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    duration: 6,
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quo exercitationem fugit, qui corporis.",
+    cost: 2100,
+    location: "china",
+  },
+  {
+    id: 6,
+    image: tour6,
+    date: "august 26th, 2020",
+    title: "Tibet Adventure",
+    duration: 6,
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quo exercitationem fugit, qui corporis.",
+    cost: 2100,
+    location: "china",
+  },
 ];

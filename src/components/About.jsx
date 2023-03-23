61a8f6e256d62b6a7d01f9adb35531f2f8b80509
sharonkorir <sharon.korir@student.moringaschool.com>
2023-03-23 14:55:32 +0300

    Refactor: create title component for services, tours and about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,12 +1,9 @@
 import about from "../assets/images/about.jpeg";
+import Title from "./Title";
 const About = () => {
   return (
     <section className="section" id="about">
-      <div className="section-title">
-        <h2>
-          about <span>us</span>
-        </h2>
-      </div>
+      <Title title="about " subTitle="us" />
 
       <div className="section-center about-center">
         <div className="about-img">
